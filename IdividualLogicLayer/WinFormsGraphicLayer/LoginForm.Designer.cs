389a49e0d7e147dc2b97dbf428826aeb683b3ec1
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-02-28 13:35:30 +0100

    Added Project Plan

--- IdividualLogicLayer/WinFormsGraphicLayer/LoginForm.Designer.cs
+++ IdividualLogicLayer/WinFormsGraphicLayer/LoginForm.Designer.cs
@@ -34,35 +34,36 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // loginlabel
             // 
             this.loginlabel.AutoEllipsis = true;
-            this.loginlabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.loginlabel.BackColor = System.Drawing.Color.Thistle;
             this.loginlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.loginlabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.loginlabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginlabel.Location = new System.Drawing.Point(453, 161);
             this.loginlabel.Name = "loginlabel";
-            this.loginlabel.Size = new System.Drawing.Size(610, 492);
+            this.loginlabel.Size = new System.Drawing.Size(610, 482);
             this.loginlabel.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(710, 205);
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(702, 201);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 39);
+            this.label1.Size = new System.Drawing.Size(135, 46);
             this.label1.TabIndex = 1;
             this.label1.Text = "Log in";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.BackColor = System.Drawing.Color.Thistle;
             this.label2.Location = new System.Drawing.Point(491, 289);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 29);
@@ -72,7 +73,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.BackColor = System.Drawing.Color.Thistle;
             this.label3.Location = new System.Drawing.Point(491, 370);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 29);
@@ -93,15 +94,30 @@
             this.textBox2.Size = new System.Drawing.Size(371, 34);
             this.textBox2.TabIndex = 5;
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(698, 474);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 53);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogin.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(698, 474);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(128, 53);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Thistle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(491, 567);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(442, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email and/or password are incorrect!";
+            this.label4.Visible = false;
             // 
             // LoginForm
             // 
@@ -109,7 +125,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WinFormsGraphicLayer.Properties.Resources.loginbg1;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
@@ -133,7 +150,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label4;
     }
 }
 
