9d6b292c3e05c8bc16be89a2ba8e5ac2f84b30f3
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-04-04 11:11:59 +0200

    Added Content details page

--- AnimeTrackerApplication/WebAppGraphic/Pages/Index.cshtml.cs
+++ AnimeTrackerApplication/WebAppGraphic/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAL.Repositories;
+using Logic.Animes;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppGraphic.Pages
@@ -6,10 +8,18 @@ namespace WebAppGraphic.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private static IAnimeRepository animeRepository = new AnimeRepository();
+        AnimeManager animeManager;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            animeManager = new AnimeManager(animeRepository);
+        }
+
+        public List<Anime> Animes
+        {
+            get { return animeManager.GetAllAnime(); }
         }
 
         public IActionResult OnGet()
