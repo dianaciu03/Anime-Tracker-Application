b72e4f96947573b47cf74f27779ce5cc14ca4a49
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-04-09 15:02:51 +0200

    Added manga manager and repository factory

---
+++ AnimeTrackerApplication/Factory/ManagerFactory/MangaManagerFactory.cs
@@ -0,0 +1,18 @@
+﻿using Logic.Animes;
+using Logic.Mangas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.ManagerFactory
+{
+    public static class MangaManagerFactory
+    {
+        public static MangaManager CreateMangaManager(IMangaRepository mangaRepository)
+        {
+            return new MangaManager(mangaRepository);
+        }
+    }
+}
---
+++ AnimeTrackerApplication/Factory/RepositoryFactory/MangaRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.RepositoryFactory
+{
+    public static class MangaRepositoryFactory
+    {
+        public static MangaRepository CreateMangaRepository()
+        {
+            return new MangaRepository();
+        }
+    }
+}
