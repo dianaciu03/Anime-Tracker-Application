c39ec64803a11fcbab27b5dc8762a438ce9766ab
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-04-26 11:27:17 +0200

    Started thinking about profile and custom lists

---
+++ AnimeTrackerApplication/Logic/Profiles/CustomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Profiles
+{
+    public class CustomList
+    {
+        private int id;
+        private string name;
+        private List<object> content;
+
+        public CustomList(string name)
+        {
+            this.name = name;
+            this.id = 0;
+            content = new List<object>();
+        }
+
+        public string Name { get { return name; } set { name = value; } }
+
+        public int Id { get { return id; } }
+
+        public List<object> Content { get { return content; } }
+    }
+}
---
+++ AnimeTrackerApplication/Logic/Profiles/Profile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Profiles
+{
+    public class Profile
+    {
+        private int id;
+        private string username;
+        private List<CustomList> customLists = new List<CustomList>();
+
+        public Profile(string username)
+        {
+            this.username = username;
+            customLists.Add(new CustomList("Watched"));
+            customLists.Add(new CustomList("Want to watch"));
+            customLists.Add(new CustomList("Dropped"));
+            customLists.Add(new CustomList("Favourite characters"));
+            customLists.Add(new CustomList("Disliked characters"));
+        }
+
+        public Profile(int id, string username, List<CustomList> customLists)
+        {
+            this.id = id;
+            this.username = username;
+            this.customLists = customLists;
+        }
+
+        public void AddCustomList(CustomList list)
+        {
+            customLists.Add(list);
+        }
+    }
+}
