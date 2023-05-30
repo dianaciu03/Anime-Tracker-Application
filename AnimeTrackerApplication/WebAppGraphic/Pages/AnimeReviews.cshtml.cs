f73c2d4a3947c48a45dd715c1d075be6a8fd1e42
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-05-31 01:55:36 +0200

    Started working on review page for anime

---
+++ AnimeTrackerApplication/WebAppGraphic/Pages/AnimeReviews.cshtml.cs
@@ -0,0 +1,42 @@
+using Logic.Animes;
+using Logic.Profiles;
+using Logic.Reviews;
+using Logic.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppGraphic.Pages
+{
+    public class AnimeReviewsModel : PageModel
+    {
+        private IAnimeManager animeManager;
+        private IUserManager userManager;
+        private IReviewManager reviewManager;
+
+        public AnimeReviewsModel(IAnimeManager animeManager, IUserManager userManager, IReviewManager reviewManager)
+        {
+            this.animeManager = animeManager;
+            this.userManager = userManager;
+            this.reviewManager = reviewManager;
+        }
+
+        public List<Review> GetReviews()
+        {
+            return reviewManager.GetAllReviews();
+        }
+
+        public Profile GetProfile()
+        {
+            int? id = HttpContext.Session.GetInt32("userId");
+            if (id != null)
+            {
+                Profile profile = userManager.GetProfileByWebUserId((int)id);
+                return profile;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
