e09223b5aa73c7295f8c40a24680855d47ea949d
Diana Ciuperca <dianav.ciuperca@gmail.com>
2023-05-31 10:01:05 +0200

    Added pages to layouts

---
+++ AnimeTrackerApplication/WebAppGraphic/Pages/ForYouManga.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppGraphic.Pages
+{
+    public class ForYouMangaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AnimeTrackerApplication/WebAppGraphic/Pages/MangaReviews.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppGraphic.Pages
+{
+    public class MangaReviewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
